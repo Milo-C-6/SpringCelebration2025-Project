8e3033481238d489092ae2be8d31d5bbafb12905
Milo <cummimil000@alpinesd.org>
2025-03-09 20:21:58 -0600

    Complete main menu, and lives, and end score

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game:
         self.resources = {}
         self.screen_width = screen_width
         self.screen_height = screen_height
-        # self.speed = 1
+        # self.speed = 1 # sadly not implemented :(
         # self.difficulty = 1
         self.current_minigame = None
         self.played_minigames = [None]
@@ -34,18 +34,26 @@ class Game:
         self.direction = 0
         self.credits = False
         self.credits_txt = [line for line in open("credits.txt","r")]
+        self.score = 0
+        self.lives = 4
+        self.lives_tick = 91
+        self.lives_y = 730
+        self.life_offset = pr.Vector2(0,0)
+        self.lost_a_life = False
+        self.game_end_score = 0
 
     def startup(self):
         pr.init_audio_device()
 
         #i love having to scroll to the end of this to add images - Milo 3/7/25
         # Alt+Z to toggle Word Wrap, makes this MUCH easier - Milo 3/8/25
-        assets = ["assets/Arrow_Up_Key_Light.png","assets/Arrow_Right_Key_Light.png","assets/Arrow_Down_Key_Light.png","assets/Arrow_Left_Key_Light.png","assets/Sewing_Monster_Doll.png","assets/Sewing_Monster_Doll_Complete.png","assets/Starburst_Explosion.png","assets/Powerwash_Gun.png","assets/Sidewalk.png","assets/Splat_1.png","assets/Splat_2.png","assets/Splat_Coffee.png","assets/Splat_Stripe.png","assets/Sparkles.png","assets/computer.png","assets/background.png","assets/electricity.png","assets/Plug.png","assets/wire.png","assets/screen1.png","assets/wire2.png","assets/connect_wire1.png","assets/connect_wire2.png","assets/Elevator.png","assets/Timer.png","assets/PCB.png","assets/Solder.png","assets/Solder_Iron.png","assets/LED_Off.png","assets/LED_On.png","assets/Check.png","assets/Important.png","assets/Wrong.png","assets/D_Key_Light.png","assets/F_Key_Light.png","assets/J_Key_Light.png","assets/K_Key_Light.png","assets/Sheet_Music_Transparent.png","assets/Guitar_1_3_7.mp3","assets/Guitar_2.mp3","assets/Guitar_4_6.mp3","assets/Guitar_5.mp3","assets/Guitar_8.mp3","assets/Guitar_9.mp3","assets/tape.png","assets/tapedWire.png","assets/lasttaskbg.png","assets/screen2.png","assets/Mouse_Left_Key_Light.png","assets/JobWare.png", "assets/Scroll.png"]
+        assets = ["assets/Arrow_Up_Key_Light.png","assets/Arrow_Right_Key_Light.png","assets/Arrow_Down_Key_Light.png","assets/Arrow_Left_Key_Light.png","assets/Sewing_Monster_Doll.png","assets/Sewing_Monster_Doll_Complete.png","assets/Starburst_Explosion.png","assets/Powerwash_Gun.png","assets/Sidewalk.png","assets/Splat_1.png","assets/Splat_2.png","assets/Splat_Coffee.png","assets/Splat_Stripe.png","assets/Sparkles.png","assets/computer.png","assets/background.png","assets/electricity.png","assets/Plug.png","assets/wire.png","assets/screen1.png","assets/wire2.png","assets/connect_wire1.png","assets/connect_wire2.png","assets/Elevator.png","assets/Timer.png","assets/PCB.png","assets/Solder.png","assets/Solder_Iron.png","assets/LED_Off.png","assets/LED_On.png","assets/Check.png","assets/Important.png","assets/Wrong.png","assets/D_Key_Light.png","assets/F_Key_Light.png","assets/J_Key_Light.png","assets/K_Key_Light.png","assets/Sheet_Music_Transparent.png","assets/Guitar_1_3_7.mp3","assets/Guitar_2.mp3","assets/Guitar_4_6.mp3","assets/Guitar_5.mp3","assets/Guitar_8.mp3","assets/Guitar_9.mp3","assets/tape.png","assets/tapedWire.png","assets/lasttaskbg.png","assets/screen2.png","assets/Mouse_Left_Key_Light.png","assets/JobWare.png", "assets/Scroll.png","assets/7segment.ttf","assets/Heart.png","assets/Heart_Broken.png"]
         iteration = 0
 
         for key in ResourceType:
-            if key.name[:5]=="SOUND":
-                print("Loaded sound")
+            if key.name[:4]=="FONT":
+                self.resources[key] = pr.load_font(assets[iteration])
+            elif key.name[:5]=="SOUND":
                 self.resources[key] = pr.load_sound(assets[iteration])
             else:
                 image = pr.load_image(assets[iteration])
@@ -71,7 +79,22 @@ class Game:
     def update(self):
         if pr.is_mouse_button_pressed(pr.MOUSE_RIGHT_BUTTON):
             print(pr.get_mouse_position().x,pr.get_mouse_position().y)
-        
+
+        if self.lives_tick != 91:
+            if self.lives_tick < 20:
+                self.lives_y -= 8.5
+            elif self.lives_tick > 70:
+                self.life_offset = pr.Vector2(0,0)
+                self.lives_y += 8.5
+                if self.lives_tick == 90: 
+                    self.lost_a_life = False
+                    if self.lives == 0: 
+                        self.reset()
+                        return
+            elif self.lives_tick > 30 and self.lives_tick < 55:
+                    self.life_offset = pr.Vector2(random.randint(-15,15),random.randint(-15,15))
+            elif self.lives_tick == 56: self.life_offset = pr.Vector2(0,0)
+            self.lives_tick+=1
         if not self.playing:
 
             if pr.check_collision_recs(pr.Rectangle(540,500,200,120),pr.Rectangle(pr.get_mouse_x(),pr.get_mouse_y(),2,2)):
@@ -84,6 +107,11 @@ class Game:
                 if pr.is_mouse_button_pressed(pr.MOUSE_LEFT_BUTTON):
                     if self.credits: self.credits = False
                     else: self.credits = True
+                    pr.set_mouse_cursor(0)
+            elif self.game_end_score > 1 and pr.check_collision_recs(pr.Rectangle(550,385,180,65),pr.Rectangle(pr.get_mouse_x(),pr.get_mouse_y(),2,2)):
+                pr.set_mouse_cursor(4)
+                if pr.is_mouse_button_pressed(pr.MOUSE_LEFT_BUTTON):
+                    self.game_end_score = 0
                     pr.set_mouse_cursor(0)
             else:
                 pr.set_mouse_cursor(0)
@@ -102,7 +130,7 @@ class Game:
                     self.transition_tick = 109
 
                 self.transition_tick+=1
-
+                
                 if self.transition_tick < 0:
                     # print(print(self.elevator_size.y))
                     # print(1/0) intentionally causing errors is a real good way to get values :D . Python debugger? Never heard of her
@@ -110,7 +138,7 @@ class Game:
                     self.door_width += 16
                     self.stopwatch_color = pr.WHITE
                 # i wish i knew about texture mode before, im going to crash out
-                pr.begin_texture_mode(self.transition)
+                pr.begin_texture_mode(self.transition) #=====================================BEGIN TEXTURE MODE (added this cuz I keep missing it)=====================================#
 
                 pr.clear_background(pr.WHITE)
                 #Door, Top left (455, 40), Top right (836,39), Bottom Right (836, 695), Bottom Left (454, 695)
@@ -125,7 +153,8 @@ class Game:
                     0,
                     pr.WHITE
                 )
-                pr.end_texture_mode()
+                pr.draw_text_ex(self.resources[ResourceType.FONT_7SEGMENT],str(self.score),pr.Vector2(293,105),70,5,pr.GREEN)
+                pr.end_texture_mode() #=====================================END TEXTURE MODE=====================================#
                 if self.transition_tick >= 110:
                     if self.transition_tick == 110:
                         if self.debug_minigame != None:
@@ -174,6 +203,11 @@ class Game:
                     self.played_minigames[0] = self.current_minigame.id.value
                     self.current_minigame = None
                     self.transition_tick = -20
+                    self.score += 1
+                    self.lives_tick = 0
+                    if self.stopwatch_color == pr.RED:
+                        self.lives-=1
+                        self.lost_a_life = True
             
             if self.text_y_tick!=61:
                 self.text_pos_y-=15
@@ -205,6 +239,34 @@ class Game:
             )
         if self.current_minigame != None:
             pr.draw_text(self.current_minigame.instruction,int(self.screen_width//2-(pr.measure_text(self.current_minigame.instruction,self.text_size)/2)),int(self.text_pos_y),self.text_size,pr.BLACK)
+        if self.lives_tick != 91:
+            for i in range(4):
+                if i+1 <= self.lives:
+                    texture = self.resources[ResourceType.TEXTURE_HEART]
+                    used_offset = pr.Vector2(0,0)
+                elif i == self.lives and self.lost_a_life:
+                    used_offset = self.life_offset
+                    if self.lives_tick > 41: 
+                        texture = self.resources[ResourceType.TEXTURE_HEART_BROKE]
+                    else:
+                        texture = self.resources[ResourceType.TEXTURE_HEART]
+                elif i+1 > self.lives:
+                    used_offset = pr.Vector2(0,0)
+                    texture = self.resources[ResourceType.TEXTURE_HEART_BROKE]
+                else:
+                    texture = self.resources[ResourceType.TEXTURE_HEART_BROKE]
+                    used_offset = pr.Vector2(0,0)
+
+                pr.draw_texture_pro(
+                    texture,
+                    pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_HEART].width,self.resources[ResourceType.TEXTURE_HEART].height),
+                    pr.Rectangle(10+used_offset.x+i*160,self.lives_y+used_offset.y,150,138),
+                    pr.Vector2(0,0),
+                    0,
+                    pr.WHITE
+                )
+
+
         if not self.playing:
             pr.draw_rectangle(0,0,1280,720,pr.Color(30,30,30,190))
             for i in range(6,-1,-1):
@@ -229,6 +291,16 @@ class Game:
                 0,
                 pr.WHITE
             )
+
+            if self.game_end_score > 0:
+                pr.draw_rectangle(440,260,400,200,pr.BLUE)
+                pr.draw_rectangle_lines_ex(pr.Rectangle(440,260,400,200),5,pr.Color(56, 88, 138,255))
+                pr.draw_text("Ending Score",470,270,50,pr.WHITE)
+                pr.draw_text(str(self.game_end_score),int(640-(pr.measure_text(str(self.game_end_score),40)/2)),330,40,pr.WHITE)
+                pr.draw_rectangle(550,385,180,65,pr.GREEN)
+                pr.draw_rectangle_lines_ex(pr.Rectangle(550,385,180,65),5,pr.Color(34, 139, 34, 255))
+                pr.draw_text("Cool!",604,400,30,pr.WHITE)
+
             if self.credits:
                 pr.draw_rectangle(100,50,1080,620,pr.BLUE)
                 pr.draw_rectangle_lines_ex(pr.Rectangle(100,50,1080,620),5,pr.Color(56, 88, 138,255))
@@ -238,10 +310,6 @@ class Game:
                         pr.draw_text(self.credits_txt[i],130,250+i*15,12,pr.WHITE)
                     else:
                         pr.draw_text(self.credits_txt[i],130,100+i*30,28,pr.WHITE)
-                # pr.draw_text('''Minigame Developement:
-                # \n\tMilo Cummings (Milo-C-6), Lincoln Dabb (LincolnDabb3)''',130,100,28,pr.WHITE)
-                # pr.draw_text("Main Menu, Transition Code:",130,160,28,pr.WHITE)
-                # pr.draw_text("Milo Cummings",150,190,28,pr.WHITE)
 
     def shutdown(self):
         for key in ResourceType:
@@ -249,3 +317,16 @@ class Game:
 
         pr.close_audio_device()
         
+    def reset(self):
+        self.game_end_score = self.score
+        self.lives = 4
+        self.score = 0
+        self.transition_tick = 0 # max 181, dont edit if you already have a debug minigame set
+        self.text_y_tick = 61
+        self.door_width = 191
+        self.lost_a_life = False
+        self.stopwatch_time = 7
+        self.text_pos_y = self.screen_height//2
+        self.text_size = 250 #40
+
+        self.playing = False
--- readme.md
+++ readme.md
@@ -2,10 +2,9 @@
 my groups project for the Ken Garff Esports Spring Celebration Coding Competition
 
 ## TODO:
-* Include Score in transition between minigames
-* Add lives
-* Add score counter
-* Make main menu
+* Do some small polishing
+    - We need a sewing needle in sewing
+    - We need a background for sewing, and soldering
 * *Add more minigames*
     - **Not as needed anymore but still would be cool if we had time**
     - first idea off the top of my head is a Welding like one, and a cooking one
