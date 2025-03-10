2b1eabe5726c5d56bf31f38bdfc19c9bcb59a707
LincolnDabb3 <dabblin000@alpinesd.org>
2025-03-10 15:19:06 -0600

    almost there just need to playtest it

--- minigames/construct.py
+++ minigames/construct.py
@@ -17,6 +17,7 @@ class Minigame:
         self.max_time = 7*max_time_multiplier
         self.instruction = "Build The House!"
         self.b1 = False
+        self.b2 = True
 
     def update(self):
         pass
@@ -24,7 +25,7 @@ class Minigame:
     def render(self):
         pr.draw_texture_pro(
                 self.resources[ResourceType.TEXTURE_BGUNDONE],
-                pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_HOUSE].width,self.resources[ResourceType.TEXTURE_HOUSE].height),
+                pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BGUNDONE].width,self.resources[ResourceType.TEXTURE_BGUNDONE].height),
                 pr.Rectangle(0,0,self.screen_width,self.screen_height/1.75),
                 (0,0),
                 0,
@@ -38,7 +39,7 @@ class Minigame:
                 1,
                 pr.WHITE
             )
-            if pr.is_mouse_button_released(0) and pr.check_collision_recs(pr.Rectangle(pr.get_mouse_position().x,pr.get_mouse_position().y,1,1),pr.Rectangle(550,0,300,300)):
+            if pr.is_mouse_button_released(0) and pr.check_collision_recs(pr.Rectangle(pr.get_mouse_position().x,pr.get_mouse_position().y,1,1),pr.Rectangle(340,0,300,300)):
                 pr.draw_texture_pro(
                     self.resources[ResourceType.TEXTURE_BLOCK1],
                     pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BLOCK1].width,self.resources[ResourceType.TEXTURE_BLOCK1].height),
@@ -47,14 +48,55 @@ class Minigame:
                     .75,
                     pr.WHITE
                 )
+                self.b1 = True
+                self.b2 = False
+
         elif not pr.is_mouse_button_down and self.b1 == False:
             pr.draw_texture_pro(
-                self.resources[ResourceType.TEXTURE_],
+                self.resources[ResourceType.TEXTURE_BLOCK1],
                 pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BLOCK1].width,self.resources[ResourceType.TEXTURE_BLOCK1].height),
                 pr.Rectangle(0,0,self.screen_width,self.screen_height/1.75),
                 (pr.get_mouse_position().x,pr.get_mouse_position().y),
                 0,
                 pr.WHITE
             )
+        
+        if pr.is_mouse_button_down and self.b2 == False:
+            pr.draw_texture_ex(
+                self.resources[ResourceType.TEXTURE_BLOCK2],
+                pr.vector2_add(pr.get_mouse_position(),pr.Vector2(-250,-300)), #should be changed for scaling! if we still care for that
+                0,
+                1,
+                pr.WHITE
+            )
+            if pr.is_mouse_button_released(0) and pr.check_collision_recs(pr.Rectangle(pr.get_mouse_position().x,pr.get_mouse_position().y,1,1),pr.Rectangle(640,0,300,300)):
+                pr.draw_texture_pro(
+                    self.resources[ResourceType.TEXTURE_BLOCK2],
+                    pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BLOCK2].width,self.resources[ResourceType.TEXTURE_BLOCK2].height),
+                    pr.Rectangle(0,0,self.screen_width,self.screen_height/1.75),
+                    (pr.get_mouse_position().x,pr.get_mouse_position().y),
+                    .75,
+                    pr.WHITE
+                )
+                self.b2 = True
+                self.win = True
 
+        elif not pr.is_mouse_button_down and self.b2 == False:
+            pr.draw_texture_pro(
+                self.resources[ResourceType.TEXTURE_BLOCK2],
+                pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BLOCK2].width,self.resources[ResourceType.TEXTURE_BLOCK2].height),
+                pr.Rectangle(0,0,self.screen_width,self.screen_height/1.75),
+                (pr.get_mouse_position().x,pr.get_mouse_position().y),
+                0,
+                pr.WHITE
+            )
+        if self.win == True:
+            pr.draw_texture_pro(
+                self.resources[ResourceType.TEXTURE_BGDONE],
+                pr.Rectangle(0,0,self.resources[ResourceType.TEXTURE_BGDONE].width,self.resources[ResourceType.TEXTURE_BGDONE].height),
+                pr.Rectangle(0,0,self.screen_width,self.screen_height/1.75),
+                (0,0),
+                0,
+                pr.WHITE
+            )
 
